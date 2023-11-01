a2ddc534fbd28308fdd87c04be2a7c3f7a6a45cd
“Smit901” <smit.dudhat@bacancy.com>
2023-11-01 12:19:28 +0530

    global style setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import styled from 'styled-components'
+import GlobalStyles from './styles/GlobalStyles'
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
 `
 
+const Input = styled.input`
+  border: 1px solid gray;
+  border-radius: 5px;
+  padding: 15px;
+`
+
 function App() {
   return (
+    <>
+    <GlobalStyles />
     <div>
       <H1>Hello World</H1>
+      <Input placeholder='Helloooo' />
     </div>
+    </>
   )
 }
 
